83ac6433401b6403479c05ac47eee9f481dd06a2
otagoitachi-pixel <otagoitachi@gmail.com>
2026-09-15 10:36:55 +0530

    Connect landing page to dashboard

--- mochimo-cloudflare-github-ready/public/js/pages/auth.js
+++ mochimo-cloudflare-github-ready/public/js/pages/auth.js
@@ -3,7 +3,7 @@ import { icon } from '../lib/icons.js';
 import { toast } from '../lib/toast.js';
 
 function renderAuth(root, { onAuthed }) {
-  let mode = 'signup';
+  let mode = new URLSearchParams(location.search).get('login') === '1' ? 'login' : 'signup';
 
   function paint() {
     root.innerHTML = `
